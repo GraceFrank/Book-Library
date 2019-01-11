f84abc3797092dbb1531a90fa5a46dd19dc9c97b
Grace Frank <frankgracey@gmail.com>
2019-01-11 16:33:30 +0100

    borrow and return book methods added to the book class

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ class Book {
         this.avilableCopies = copies;
         this.borrowers = []
     }
+    borrow(userName) {
+        if (this.avilableCopies >= 1 && this.borrowers.includes(userName) === false) {
+            this.avilableCopies--;
+            this.borrowers.push(userName);
+            return `book suscesfullly borrowed by ${userName}`;
+        } else {
+            return 'book Taken'
+        } //end of else statement
+    } //end of if borrow function
+
+    returnBook(userName) {
+        if (this.borrowers.includes(userName)) {
+            this.avilableCopies++;
+            const indexOfUserName = this.borrowers.indexOf(userName);
+            this.borrowers.splice(indexOfUserName, 1)
+            return `book suscesfullly returned by ${userName}`;
+        } else {
+            return 'Book already returned'
+        } //end of else statement
+    } //end of return book function
 
 
 
