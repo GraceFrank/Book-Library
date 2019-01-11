60d12091794e76ece33f231c072a0d86b0d7dbb6
Grace Frank <frankgracey@gmail.com>
2019-01-11 16:29:53 +0100

    wip: added the book class with constructor

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-
-
-
-
-
-
-
-
-
-
-
-
-    module.exports = {
-        bookConstrutor: Book,
+class Book {
+    constructor({
+        title,
+        author,
+        isbn,
+        copies,
+    }) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.copies = copies;
+        this.avilableCopies = copies;
+        this.borrowers = []
     }
+
+
+
+
+} //end of book class
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    bookConstrutor: Book,
+}
