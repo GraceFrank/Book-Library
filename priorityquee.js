7c6e83dace7541ffc16fa6e4e44357a05b01c600
Grace Frank <frankgracey@gmail.com>
2019-01-11 16:50:55 +0100

    refactored Book class, used sets for stroing borrowers instead of arrays

---
+++ priorityquee.js
@@ -0,0 +1,15 @@
+const queue = [
+    ['dara', 1],
+    ['grace', 2],
+    ['vicky', 3],
+    ['twin', 2],
+    ['stella', 1]
+];
+
+
+queue.sort(function (a, b) {
+    return a[1] - b[1];
+});
+
+
+console.log(queue);
