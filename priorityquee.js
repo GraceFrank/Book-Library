6eca38216361843c527813e2da4c003a6785ff9e
Grace Frank <frankgracey@gmail.com>
2019-01-12 09:15:51 +0100

    WIP: added the person, teacher, student , seniorstudent and junior student clases

--- priorityquee.js
+++ priorityquee.js
@@ -1,16 +1,72 @@
 const queue = [
-    ['dara', 1],
-    ['grace', 2],
-    ['vicky', 3],
-    ['twin', 2],
-    ['stella', 1]
+
 ];
 
 
-queue.sort(function (a, b) {
-    return a[1] - b[1];
-});
 
 
-console.log(queue);
 
+
+
+class Person {
+    constructor(name) {
+        this.name = name;
+
+
+    }
+    borrow(book) {
+        queue.push(this)
+    }
+    returnB(book) {
+        console.log(`${book} book returned by ${this.name}`)
+    }
+}
+
+class Teacher extends Person {
+    constructor(name) {
+        super(name)
+        this.priority = 1
+        this.position = 'Teacher'
+    }
+}
+
+class Student extends Person {
+    constructor(name, ) {
+        super(name)
+        this.position = 'Student'
+
+    }
+}
+
+class SeniorStudent extends Student {
+    constructor(name) {
+        super(name)
+        this.position = 'SeniorStudent'
+        this.priority = 2
+    }
+}
+
+class JuniorStudent extends Student {
+    constructor(name) {
+        super(name)
+        this.position = 'JuniorStudent'
+        this.priority = 3
+    }
+}
+
+// const dareLawal = new Teacher('dareLawal');
+// const graceFrank = new SeniorStudent('graceFrank');
+// const vickyFrank = new JuniorStudent('vickyFrank')
+// console.log(dareLawal.borrow('george of the jungle'));
+// console.log(graceFrank.borrow('george of the jungle'));
+// console.log(vickyFrank.borrow('george of the jungle'));
+
+
+
+// // queue.sort(function (a, b) {
+// //     return a.priority - b.priority;
+// // });
+
+// // console.log(queue);
+
+console.log(queue)
