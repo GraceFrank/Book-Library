d5a410697b74a24fe8e90e6299f9fedf6148b293
Grace Frank <frankgracey@gmail.com>
2019-01-16 14:33:51 +0100

    added database of books

---
+++ data/books.js
@@ -0,0 +1,19 @@
+const books = {
+    amazon: {
+        title: 'amazon',
+        author: 'ak sharma',
+        copies: 2
+    },
+    netflix: {
+        title: 'netflix',
+        author: 'julia roberts',
+        copies: 3
+    },
+    microsoft: {
+        title: 'microsoft',
+        author: 'sandra bullocks',
+        copies: 4
+    }
+}
+
+
