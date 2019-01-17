c2a19f4edbd6265995da49c6085a06b9f6b77a18
Grace Frank <frankgracey@gmail.com>
2019-01-17 14:26:09 +0100

    added the person class and its test cases

--- users/person.js
+++ users/person.js
@@ -1,4 +1,4 @@
-
+const queue = require('../data/queue')
 
 class Person{
     constructor(userName) {
@@ -7,10 +7,22 @@ class Person{
     }
 
     request(bookTitle) {
+        queue.push([this, bookTitle]);
         
     }
 
     returnBook(bookTitle) {
+        this['card'].delete(bookTitle);
         
     }
 }
+
+const aondofa = new Person('aondofa');
+
+aondofa.request('amazon');
+console.log(queue);
+console.log(queue.includes([aondofa, 'amazon']));
+
+
+
+module.exports = Person;
