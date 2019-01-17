0279164bc19a54d5bf9b6241ec437ff80f7d7afe
Grace Frank <frankgracey@gmail.com>
2019-01-17 15:41:30 +0100

    added test cases for teacher class

---
+++ users/teachers/teachers.test.js
@@ -0,0 +1,24 @@
+const Teacher = require('./teacher');
+const Person = require('../person');
+const queue = require('../../data/queue');
+
+const kingsley = new Teacher('kingsley');
+
+describe('test that teacher inherits properties and methods of person', () => {
+    test('that teacher has property card', () => {
+        expect(kingsley['card']).toBeTruthy();
+    }); 
+      test('that request function adds bookTitle to queue', () => {
+          kingsley.request('amazon');
+          expect(queue[0].includes('amazon')).toBeTruthy();
+      });
+      test('that request function adds person to queue', () => {
+          kingsley.request('amazon');
+          expect(queue[0][0]).toEqual(kingsley);
+      });
+        test('that return function removes book from user card', () => {
+            kingsley.request('amazon');
+            kingsley.returnBook('amazon');
+            expect(kingsley['card'].has('amazon')).toBeFalsy();
+        });
+});
